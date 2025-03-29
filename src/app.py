1642c0b0b35e457aa21bc7f78ca4c99f7765343e
ValentinaBassSkrivanelli <valswaonyou@gmail.com>
2025-03-29 19:29:55 -0300

    change

--- src/app.py
+++ src/app.py
@@ -2,38 +2,42 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 import openai
+
+# Cargar variables de entorno
 load_dotenv()
 
+# Configurar clave de la API de OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Título de la app
 st.title("Generador de Imágenes de Valen")
 
-
-# Inicia el formulario
+# Formulario de entrada
 with st.form("images_form"):
-    # Agrega un campo de entrada para el texto
     text = st.text_input("Texto para generar imagen")
-    
-    # Agrega un campo para seleccionar el número de imágenes a generar
     num_images = st.number_input("Número de imágenes a generar", min_value=1, max_value=10, value=1)
-    
-    # Agrega un campo para seleccionar el tamaño de la imagen
     image_size = st.selectbox("Tamaño de la imagen", ["256x256", "512x512", "1024x1024"], index=1)
-    
-    # Agrega el botón de envío del formulario
     submit_button = st.form_submit_button(label="Generar Imágenes")
 
-# Procesa el formulario cuando se envíe
+# Procesar formulario
 if submit_button:
-    # Aquí puedes realizar cualquier acción que desees cuando se envíe el formulario
-  st.write("Generando Imagenes...")
+    if text.strip():  # Verifica que el usuario haya ingresado un texto
+        st.write("Generando imágenes...")
 
-  response = openai.Image.create(
-    prompt=text,
-    n=num_images,
-    size= image_size
-    )
+        try:
+            response = openai.images.generate(
+                prompt=text,
+                n=num_images,
+                size=image_size
+            )
 
-for i in range(num_images):
-   url = response.data[i].url
-   st.image(url,caption=f"Imagenn {i+1}", use_column_width=True)
+            # Mostrar las imágenes generadas
+            for i, image_data in enumerate(response.data):
+                url = image_data.url
+                st.image(url, caption=f"Imagen {i+1}", use_column_width=True)
+
+        except Exception as e:
+            st.error(f"Error al generar imágenes: {e}")
+
+    else:
+        st.warning("Por favor, ingresa un texto para generar las imágenes.")
